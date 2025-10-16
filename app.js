f57c4a892a7469623fc2c833e88f67af470a255d
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-10-16 12:12:00 -0300

    up

---
+++ app.js
@@ -0,0 +1,9 @@
+const pokemon_number = document.getElementById ("pkun");
+const pokemon_name = document.getElementById ("pkname");
+const pokemon_img = document.getElementById ("pkimg");
+
+const pokemon_form = document.getElementById ("form");
+const pokemon_input = document.getElementById ("input");
+
+const pokemon_prev = document.getElementById ("bnt-prev");
+const pokemon_next = document.getElementById ("bnt-next");
