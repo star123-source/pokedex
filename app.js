64d96c33610f511e8e77f4118a174fc70f04746e
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-10-21 08:23:23 -0300

    up

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ const pokemon_input = document.getElementById ("input");
 
 const pokemon_prev = document.getElementById ("bnt-prev");
 const pokemon_next = document.getElementById ("bnt-next");
+
+let searchPokemon = 1;
+
+const fetchpokemon = async (pokemon) => {
+    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon`)
+        if(APIResponse.status ===200){
+            const data = await APIResponse.json();
+        }
+}
